113b3c15c90b2b9a71e8a3fa8bef55fddbb95cb8
telecsur <cmstmayo@gmail.com>
2020-03-16 17:49:09 +0000

    Añadido objeto urls con las uris de las rutas de los recursos del servicio web. En los métodos se hace referencia a este objeto en vez de harcodea.r el texto en cada uno de ellos (buenas prácticas)

--- Cristina/service_REST_timber/config/routes.js
+++ Cristina/service_REST_timber/config/routes.js
@@ -8,35 +8,50 @@ const imageController = require('../controllers/imageController');
 
 const api = express.Router();
 
+var urls = {
+    root: '/',
+    users: '/users',
+    user: '/users/:userId',
+    trees: '/trees',
+    tree: '/trees/:treeId',
+    annotations: '/annotations',
+    annotation: '/annotations/:annotationId',
+    images: '/images',
+    image: '/images/:imagesId'
+};
+
+//Root
+api.get(urls.root, (req, res) => {
+    res.status(200).send(urls);
+});
 
 //Usuarios
 /*api.get('/users', auth, userController.getUsers)
 api.get('/users/:userId', userController.getUser)
 api.put('/users/:userId', auth, userController.createUpdateUser) //Si existe se actualiza
-api.delete('/users/:userId', auth, userController.deleteUser)
-*/
+api.delete('/users/:userId', auth, userController.deleteUser)*/
 
 //Árboles
-api.get('/trees', treeController.getTrees);
-/*api.post('/trees', treeController.createTree);
-api.get('/trees/:treeId', treeController.getTree);
-api.put('/trees/:treeId', treeController.updateTree);
-api.delete('/trees/:treeId', treeController.deleteTree);
+api.get(urls.trees, treeController.getTrees);
+//api.post(urls.trees, treeController.createTree);
+/*api.get(urls.tree, treeController.getTree);
+api.put(urls.tree, treeController.updateTree);
+api.delete(urls.tree, treeController.deleteTree); 
 */
 
 //Anotaciones
-api.get('/annotations', annotationController.getAnnotations);
-/* api.post('/annotations', annotationController.createAnnotation);
-api.get('/annotations/:annotationId', annotationController.getAnnotation);
-api.put('/annotations/:annotationId', annotationController.updateAnnotation);
-api.delete('/annotations/:annotationId', annotationController.deleteAnnotation);
+api.get(urls.annotations, annotationController.getAnnotations);
+/* api.post(urls.annotations, annotationController.createAnnotation);
+api.get(urls.annotation, annotationController.getAnnotation);
+api.put(urls.annotation, annotationController.updateAnnotation);
+api.delete(urls.annotation, annotationController.deleteAnnotation);
 
 //Imágenes y partes
-api.get('/images', imageController.getImages);
-api.post('/images', imageController.createImage);
-api.get('/images/:imagesId', imageController.getImage);
-api.put('/images/:imagesId', imageController.updateImage);
-api.delete('/images/:imagesId', imageController.deleteImage);
+api.get(urls.images, imageController.getImages);
+api.post(urls.images, imageController.createImage);
+api.get(urls.image, imageController.getImage);
+api.put(urls.image, imageController.updateImage);
+api.delete(urls.image, imageController.deleteImage);
 api.get('/images/:imagesId/parts', imageController.getImageParts);
 api.post('/images/:imagesId/parts', imageController.createImagePart);
 api.get('/images/:imagesId/parts/:partId', imageController.getImagePart);
