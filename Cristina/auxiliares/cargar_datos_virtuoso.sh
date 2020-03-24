bd229ad1609b45a8220abce27f7cf75e62789739
telecsur <cmstmayo@gmail.com>
2020-03-24 12:45:20 +0100

    *Añadidos cambios para cargar los nuevos ficheros de datos en virtuoso. *Corregido error en ruta.

--- Cristina/auxiliares/cargar_datos_virtuoso.sh
+++ Cristina/auxiliares/cargar_datos_virtuoso.sh
@@ -5,6 +5,9 @@ port=1111
 ruta_git_data="$HOME/SocialTreeAnnotation/Cristina/data"
 ruta_git_onto="$HOME/SocialTreeAnnotation/Cristina/ontology"
 fichero_datos="example.ttl"
+fichero_datos1="annotations_example.ttl"
+fichero_datos2="trees_example.ttl"
+
 fichero_ontologia="ontology.ttl"
 fichero_ifn="ifn-core.ttl"
 fichero_mfe="mfe-core.ttl"
@@ -13,17 +16,27 @@ grafo_crossforest="http://timber.gsic.uva.es/crossforest"
 
 
 echo -e "Copiando ficheros de git a directorio modelo_datos para el docker virtuoso"
-if [ ! -f $HOME/data/modelo_datos/$fichero_datos ]; then
+if [ ! -f $HOME/data/virtuoso/modelo_datos/$fichero_datos ]; then
     echo "El fichero $HOME/data/modelo_datos/$fichero_datos no existía, se crea"
-    touch $HOME/data/modelo_datos/$fichero_datos
+    touch $HOME/data/virtuoso/modelo_datos/$fichero_datos
 fi
-if [ ! -f touch $HOME/data/modelo_datos/$fichero_ontologia ]; then
+if [ ! -f $HOME/data/virtuoso/modelo_datos/$fichero_datos1 ]; then
+    echo "El fichero $HOME/data/modelo_datos/$fichero_datos1 no existía, se crea"
+    touch $HOME/data/virtuoso/modelo_datos/$fichero_datos1
+fi
+if [ ! -f $HOME/data/virtuoso/modelo_datos/$fichero_datos2 ]; then
+    echo "El fichero $HOME/data/modelo_datos/$fichero_datos2 no existía, se crea"
+    touch $HOME/data/virtuoso/modelo_datos/$fichero_datos2
+fi
+if [ ! -f $HOME/data/virtuoso/modelo_datos/$fichero_ontologia ]; then
     echo "El fichero $HOME/data/modelo_datos/$fichero_ontologia no existía, se crea"
-    touch touch $HOME/data/modelo_datos/$fichero_ontologia
+    touch $HOME/data/virtuoso/modelo_datos/$fichero_ontologia
 fi
 
-cp $ruta_git_data/$fichero_datos $HOME/data/modelo_datos/$fichero_datos
-cp $ruta_git_onto/$fichero_ontologia $HOME/data/modelo_datos/$fichero_ontologia
+cp $ruta_git_data/$fichero_datos $HOME/data/virtuoso/modelo_datos/$fichero_datos
+cp $ruta_git_data/$fichero_datos1 $HOME/data/virtuoso/modelo_datos/$fichero_datos1
+cp $ruta_git_data/$fichero_datos2 $HOME/data/virtuoso/modelo_datos/$fichero_datos2
+cp $ruta_git_onto/$fichero_ontologia $HOME/data/virtuoso/modelo_datos/$fichero_ontologia
 
 echo -e "----------- Borrando ficheros -----------\n"
 docker exec -it virtuoso_db_1 isql-v $port $user $pass exec="RDF_GLOBAL_RESET ();" 
@@ -33,6 +46,8 @@ echo -e "\n Borrado completo; \n "
 
 echo -e "\n\n----------- Cargando ficheros nuevos -----------\n"
 docker exec -it virtuoso_db_1 isql-v $port $user $pass exec="ld_dir('./modelo_datos', '$fichero_datos', '$grafo');"
+docker exec -it virtuoso_db_1 isql-v $port $user $pass exec="ld_dir('./modelo_datos', '$fichero_datos1', '$grafo');"
+docker exec -it virtuoso_db_1 isql-v $port $user $pass exec="ld_dir('./modelo_datos', '$fichero_datos2', '$grafo');"
 docker exec -it virtuoso_db_1 isql-v $port $user $pass exec="ld_dir('./modelo_datos', '$fichero_ontologia', '$grafo');"
 docker exec -it virtuoso_db_1 isql-v $port $user $pass exec="ld_dir('./modelo_datos', '$fichero_ifn', '$grafo_crossforest');"
 docker exec -it virtuoso_db_1 isql-v $port $user $pass exec="ld_dir('./modelo_datos', '$fichero_mfe', '$grafo_crossforest');"
