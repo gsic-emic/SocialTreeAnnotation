2e2307f444cad7a5d78422b1b780d277da141c3b
jimenaandrade <jimena.andrade@hotmail.com>
2020-05-25 11:52:22 +0200

    movimiento en el mapa

--- Jimena/src/app/inicio/inicio.component.ts
+++ Jimena/src/app/inicio/inicio.component.ts
@@ -17,47 +17,17 @@ export class InicioComponent implements OnInit {
 
   //-----------------------------
   mapa: boolean = true;
-  actualUrl: string = "http://localhost:8888/sta/data/tree";
-  url_zona: string = "http://localhost:8888/sta/data/tree?lat0=41.7&long0=-5.4&lat1=41.8&long1=-4.9"
-  //url_zona: string = "http://localhost:8888/sta/data/tree?lat0=43.2&long0=-2.9&lat1=43.3&long1=-2.7"
 
-  nextUrl: string;
-  objTrees: Tree[]; // Objeto JSON que almacena todos los árboles devueltos
-  trees: Tree[]=[]; // Array con todos los árboles del sistema con formato adecuado para visualización
   error: boolean = false;
-  terminado: boolean = false;
   terminado_species: boolean = false;
   objSpecies: object=[];
-  buscadorSpecies: string = "http://crossforest.eu/ifn/ontology/vulgarName";
-  buscadorUri: string = "uri";
   
 
   ngOnInit(): void {
-    this.getSpecies(); // nada más cargarse que recoja las especies, dentro de esta funcion ya se llama a la de getTrees 
+    this.getSpecies(); // nada más cargarse que recoja las especies
  }
 
   //-----------------------------
-  // Recojo el JSON con todos los árboles y la URL a la siguiente página mediante la funcion GET del servicio
-  getTrees(url: string){
-    this.api.getTrees(url).subscribe(
-      (data: any) =>{
-        //this.nextUrl = data.nextPage.url;
-        this.objTrees = data.response; // si la consulta se realiza con éxito, guardo los datos que me devuelve
-        //console.log(data);
-        this.convertirDato();
-      },
-      (error) =>{
-        console.error(error); // si se ha producido algún error
-        this.error = true;
-        alert("Ha habido un error al intentar cargar los árboles del sistema. Por favor, inténtelo de nuevo más tarde o recargue la página");
-        this.terminado = true;
-      },
-      () =>{
-        this.terminado = true;
-      }
-      );
-  }
-
   getSpecies(){
     this.api.getSpecies().subscribe(
       (data: any) =>{
@@ -70,36 +40,10 @@ export class InicioComponent implements OnInit {
         alert("Ha habido un error al intentar cargar las especies del sistema.");
         //this.terminado_species = true;
       },
-      () =>{  // una vez que tengo las especies, pedo llamar a la funcion que obtiene los árboles
-        this.getTrees(this.url_zona);
+      () =>{
+        this.terminado_species = true;
       }
       );
-  }
-  // método que  crea un objeto de tipo Tree
-  createTree(id: string, latitud: number, longitud: number, specie: string, creator: string, date: string){
-    let tree = { id: id, lat: latitud, long: longitud, species: specie, creator:  creator, date: date};
-    //console.log("Se ha creado el siguiente árbol: "+ tree.id +":"+tree.lat, tree.long);
-    return tree;
-  }
-  // creo un objeto TypeScritp de tipo Tree[] con el objeto JSON devuelto para mostrarse en la pantalla adecuadamente
-  convertirDato(){
-    let i=0;
-    for (let clave in this.objTrees){
-      if (this.objTrees[clave].creator == "http://crossforest.eu/ifn/ontology/")
-        { 
-          this.objTrees[clave].creator = "IFN"
-        } 
-      // añado nombre vulgar a la especie
-      for (let clav in this.objSpecies){
-        if(this.objTrees[clave].species == this.objSpecies[clav]["uri"]){
-          this.objTrees[clave].species = this.objSpecies[clav][this.buscadorSpecies]["lits"].es;
-          break;
-        }
-      }
-      this.objTrees[clave].date = "1/1/2020";
-      this.trees[i] = this.createTree(clave, this.objTrees[clave].lat, this.objTrees[clave].long, this.objTrees[clave].species, this.objTrees[clave].creator, this.objTrees[clave].date);
-      i++;
-    }
   }
   //----------------------------------------------------------
 }
--- Jimena/src/app/mapa/mapa.component.ts
+++ Jimena/src/app/mapa/mapa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Tree} from '.././tree';
+import {APIService} from '../api.service';
 
 
 declare let L;
@@ -11,49 +12,153 @@ declare let L;
 })
 export class MapaComponent implements OnInit {
 
-  @Input() trees: Tree[]; // Los árboles a mostrar llegan como parámetro de entrada desde la lista completa
-   cargando: boolean = false; // controla el spinner de "cargando"
+  @Input() objSpecies; // Las especies se cargan en el componente padre
+  @Input() mapa;
+  //-----------------------------
+  
    muestra_info: boolean = false;
+   movido: boolean = false;
 
-  constructor() { }
+   // Creación del objeto mapa
+   mymap;// mapa centrado en españa 
+
+  // Variables para la comunicacion con la API ----------------------------------
+  buscadorSpecies: string = "http://crossforest.eu/ifn/ontology/vulgarName";
+  objTrees: Tree[]; // Objeto JSON que almacena todos los árboles devueltos
+  trees: Tree[]=[]; // Array con todos los árboles del sistema con formato adecuado para visualización
+  error: boolean = false;
+  terminado: boolean = false;
+  buscadorUri: string = "uri";
+  nohay: boolean = false;
+  ver: boolean = false;
+
+   // Coordenadas del area que abarca el mapa en cada momento
+   lat0: number;
+   long0: number;
+   lat1: number;
+   long1: number;
+
+  constructor(private api: APIService) { }
 
   ngOnInit(): void { /*se ejecuta en cuanto angular tenga listo el componente*/
-    /* Creación del objeto mapa y su capa de diseño */
-    var mymap = this.crearMapa(41.763, -5.1093, 13); // mapa centrado en españa 
-    var layer = this.crearLayer_gray();
-    layer.addTo(mymap);
 
-    if (!this.trees){
-      this.cargando = true;
+    /* Creación del objeto mapa y su capa de diseño */
+    this.mymap = this.crearMapa(41.763, -5.1093, 13); // mapa centrado en españa 
+    var layer = this.crearLayer_gray();
+    layer.addTo(this.mymap);
+
+    // Inluyo botón de localización con Leaflet (ver https://github.com/domoritz/leaflet-locatecontrol))
+    L.control.locate({
+      icon: 'fas fa-street-view',
+      flyTo: true,
+      showPopup: false,
+      locateOptions: { maxZoom: 15, animate: true, duration: 1 },
+      strings: {
+        title: "Ver mi ubicación"
     }
+    }).addTo(this.mymap);
+
+    // Al iniciar la app, se carga el mapa donde está centrado
+      var bounds = this.mymap.getBounds(); // Obtengo las coordenadas area que abarca el mapa al cargarse
+      this.lat1 = bounds._northEast.lat;
+      this.long1 = bounds._northEast.lng;
+      this.lat0 = bounds._southWest.lat;
+      this.long0 = bounds._southWest.lng;
+      console.log(bounds);
+    this.actualizarTrees(this.lat0, this.long0, this.lat1, this.long1);
+
+
+    
+
+    // DETECCIÓN DE CAMBIOS EN EL MAPA
+    // Evento que detecta cuando el mapa ha dejado de moverse      
+     this.mymap.on('moveend', () =>{
+      this.terminado = false;
+      this.nohay = false;
+      var bounds = this.mymap.getBounds(); // Obtengo las coordenadas del nuevo area que abarca el mapa
+      this.lat1 = bounds._northEast.lat;
+      this.long1 = bounds._northEast.lng;
+      this.lat0 = bounds._southWest.lat;
+      this.long0 = bounds._southWest.lng;
+      this.trees = []; // Borro los datos de los árboles que tenia guardados
+      this.actualizarTrees(this.lat0, this.long0, this.lat1, this.long1); // almaceno los nuevos
+      this.pintarArboles();
+      console.log("Se ha movido el mapa");
+     });
+     
 
     // Añado los marcadores de los árboles registrados
-    var treeIcon = this.crearIcono();
-    for (var i = 0; i <= this.trees.length; i++) {
-      L.marker([this.trees[i].lat, this.trees[i].long], {icon: treeIcon}).
-      addTo(mymap).bindPopup(
-        '<div class="card" style="width: 12rem;"><div class="card-body"><h5 class="card-title"><i class="fab fa-pagelines"></i> '+this.trees[i].species + 
-        '</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-user"></i> Creador: ' + this.trees[i].creator +
-        '</h6><p class="card-text"><i class="far fa-clock"></i></p><button type="button" class="btn btn-primary ml-4">Ver más</button></div></div>');
-   }
-   
+    this.pintarArboles();
+
   }
+
   mostrar_info(){
     this.muestra_info = true;
   }
+
+//---------------- Funciones relacionadas con la obtencion de los arboles -------------------------------
+  // Consulta a la api para que me devuelva el JSON con todos los árboles en la zona que muestra el mapa
+  actualizarTrees(lat0: number, long0: number, lat1: number, long1: number){
+    this.api.getTreesZone(lat0, long0, lat1, long1).subscribe(
+        (data: any) =>{
+          if (data != null){
+            this.objTrees = data.response; // si la consulta se realiza con éxito, guardo los datos que me devuelve
+            //console.log(data.response);
+            this.convertirDato();
+          }else{
+            console.log("No hay árboles en la zona seleccionada");
+            this.nohay = true;
+          }
+        },
+        (error) =>{
+          console.error(error); // si se ha producido algún error
+          this.error = true;
+          alert("Ha habido un error al intentar cargar los árboles del sistema. Por favor, inténtelo de nuevo más tarde o recargue la página");
+          this.terminado = true;
+        },
+        () =>{
+          this.terminado = true;
+          console.log("se han cargado todos los arboles");
+        }
+        );
+  }
+  // creo un objeto TypeScritp de tipo Tree[] con el objeto JSON devuelto para mostrarse en la pantalla adecuadamente
+  convertirDato(){
+    let i=0;
+    for (let clave in this.objTrees){
+      if (this.objTrees[clave].creator == "http://crossforest.eu/ifn/ontology/")
+        { 
+          this.objTrees[clave].creator = "IFN"
+        } 
+      // añado nombre vulgar a la especie
+      for (let clav in this.objSpecies){
+        if(this.objTrees[clave].species == this.objSpecies[clav]["uri"]){
+          this.objTrees[clave].species = this.objSpecies[clav][this.buscadorSpecies]["lits"].es;
+          break;
+        }
+      }
+      this.objTrees[clave].date = "1/1/2020";
+      this.trees[i] = { id: clave, lat: this.objTrees[clave].lat, long: this.objTrees[clave].long, species: this.objTrees[clave].species, creator:  this.objTrees[clave].creator, date: this.objTrees[clave].date};
+      i++;
+    }
+    this.pintarArboles();
+  }
+  //----------------------------------------------------------
   
 
-  /* Creación del objeto mapa*/
+  
+  //---------------- Funciones relacionadas con el mapa -----------------------------------------
+  // Creación del objeto mapa
   crearMapa (lat:number, long:number, zoom:number){
     var mymap = L.map('mapid').setView([lat, long], zoom); /* creo el objeto mymap */
     return mymap;
   }
 
-  /* Creación de las capas del mapa */
+  // Creación de las capas del mapa 
   crearLayer_gray(){
     var grayscale = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-        maxZoom: 20,
-        minZoom: 5,
+        maxZoom: 22,
+        minZoom: 12,
 	      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
       });
     return grayscale;
@@ -86,15 +191,20 @@ export class MapaComponent implements OnInit {
     var marker = L.marker([latitud, longitud]);
     return marker;
   }
-
-  /* Función que crea una variable para añadir un circulo en la latitud y longitud especificadas */
-  addCircleGreen (latitud:number, longitud:number){
-    var circle = L.circle([latitud, longitud], {
-     color: 'green',
-     fillColor: '#088A08',
-     fillOpacity: 0.35,
-     radius: 500
-    });
-    return circle;
+  // Función que crea un icono para cada arbol cargado en el mapa
+  pintarArboles(){
+    var treeIcon = this.crearIcono();
+    for (var i = 0; i <= this.trees.length; i++) {
+      L.marker([this.trees[i].lat, this.trees[i].long], {icon: treeIcon}).
+      addTo(this.mymap).bindPopup(
+        '<h5 class="card-title"><i class="fab fa-pagelines"></i> '+this.trees[i].species + 
+        '</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-user"></i> Creador: ' + this.trees[i].creator +
+        '</h6><p class="card-text"><i class="far fa-clock"></i> '+this.trees[i].date+'</p><button type="button" (onclick)="verArbol('+ this.trees[i] + ')"; class="btn btn-primary ml-4">Ver más</button>');
+   }
   }
+  verArbol(tree: Tree){
+    console.log("Se ha pinchado un arbolito con id="+tree.id);
+  }
+ 
+ 
 }
