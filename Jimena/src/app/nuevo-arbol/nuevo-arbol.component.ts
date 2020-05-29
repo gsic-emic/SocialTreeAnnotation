ce7171d450e821b367834963c5acdeeec7124546
jimenaandrade <jimena.andrade@hotmail.com>
2020-05-29 12:31:35 +0200

    Inclusion de código para extraer metadatos (NO FUNCIONA)

--- Jimena/src/app/nuevo-arbol/nuevo-arbol.component.ts
+++ Jimena/src/app/nuevo-arbol/nuevo-arbol.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Tree_complete } from '../tree_complete';
 import { APIService} from '../api.service';
+//import {EXIF as exifShim, EXIFStatic } from '../../../node_modules/exif-js/exif';
 
+//declare var EXIF : EXIFStatic;
 
 @Component({
   selector: 'app-nuevo-arbol',
@@ -43,6 +45,12 @@ export class NuevoArbolComponent implements OnInit {
   description: string;
   PARTES: Array<string> = ["Parte del árbol", "Tronco", "Otra parte", "Hoja", "Vista general", "Fruto", "Flor", "Copa", "Rama"]
   depicts: string;
+  metadata: Array<any>;
+  date_img: string;
+  lat_img: number;
+  long_img: number;
+  width: number;
+  heigth: number;
 
   constructor(private api: APIService) { }
 
@@ -206,6 +214,7 @@ export class NuevoArbolComponent implements OnInit {
 
   /******* Conversión de las imágenes a base64 para madar al servidor */
   selectFile(event){
+    var metadata = {};
     var files = event.target.files;
     var file = files[0];
 
@@ -213,6 +222,18 @@ export class NuevoArbolComponent implements OnInit {
         var reader = new FileReader();
         reader.onload =this.codeFile.bind(this);
         reader.readAsBinaryString(file);
+
+       
+      // Extraigo los metadatos de la imagen
+      /*this.setDataImage(file).then((exifdata) => {
+        Object.keys(exifdata).forEach((prop) => {
+          if(prop != undefined){
+              metadata[prop] = exifdata[prop];
+          }
+        });
+      });
+      console.log(metadata);
+      this.convertirMetadata(metadata);*/
     }
   }
 
@@ -227,7 +248,85 @@ codeFile(event) {
       this.base64 = null;
       this.imagen = null;
     }
+
   }
+
+  /****************************** METADATOS *******************************/
+  /*
+  setDataImage(image_path){
+     // Consigo los metadatos de la imagen
+     return new Promise((resolve, reject) => {
+      //console.log(image_path)
+      this.getExifData(image_path).then((metadata) => {
+        //Aqui va en forma de promise o callbak 
+        resolve(metadata);
+        
+      })
+    });
+
+  }
+
+  // Cargo los metadatos de la imagen: extraído de https://github.com/exif-js/exif-js
+  getExifData(image) {
+    var exif_metadata = {};
+    var gps = {};
+
+    return new Promise((resolve, reject) => {
+
+      EXIF.getData(image, function(){
+        var allMetaData = EXIF.getAllTags(this);
+        
+        if (allMetaData != undefined) { // compruebo si hay metadatos
+          exif_metadata["DateTime"] = (allMetaData.DateTime != undefined) ? allMetaData.DateTime : 0;
+
+          exif_metadata["GPSLatitude"] = (allMetaData.GPSLatitude != undefined) ? allMetaData.GPSLatitude : null;
+          exif_metadata["GPSLatitudeRef"] = (allMetaData.GPSLatitudeRef != undefined) ? allMetaData.GPSLatitudeRef : null;
+          exif_metadata["GPSLongitude"] = (allMetaData.GPSLongitude != undefined) ? allMetaData.GPSLongitude : null;
+          exif_metadata["GPSLongitudeRef"] = (allMetaData.GPSLongitudeRef != undefined) ? allMetaData.GPSLongitudeRef : null;
+
+          //console.log(allMetaData);
+        }
+
+        resolve(exif_metadata);
+
+    });
+    
+    })
+}
+convertirMetadata(metadata){
+  if (metadata["DateTime"] != 0){
+    this.date_img = metadata.DateTime;
+    console.log(metadata);
+  }
+
+  if(metadata["GPSLatitude"] != 0 && metadata["GPSLatitudeRef"] != 0 && metadata["GPSLongitude"] != 0 && metadata["GPSLongitudeRef"] != 0 ){
+        var coordenadas = [];
+        var lat = [];
+
+     //console.log(metadata);
+
+        //coordenadas = this.getCoordinates(metadata["GPSLatitude"], metadata["GPSLatitudeRef"], metadata["GPSLongitude"], metadata["GPSLongitudeRef"]);
+        //this.lat_img = coordenadas[0];
+        //this.long_img = coordenadas[1];
+        console.log("Coordenadas: "+coordenadas[0]+" y "+ coordenadas[1]);
+  }
+}
+getCoordinates(lat, long, latRef, longRef) {
+  var coord = [];
+  coord[0] = this.ConvertDMSToDD(lat[0], lat[1], lat[2], latRef);
+  coord[1] = this.ConvertDMSToDD(long[0], long[1], long[2], longRef);
+  return coord;
+}
+
+ConvertDMSToDD(degrees, minutes, seconds, direction) {
+  var dd = degrees + minutes/60 + seconds/(60*60);
+
+  if (direction == "S" || direction == "W") {
+      dd = dd * -1;
+  } // Don't do anything for N or E
+  return dd;
+}*/
+
 
   //-----------------------------------------------------
   
