40866adea109dc0d302c882dbd74874c90ff270f
jimenaandrade <jimena.andrade@hotmail.com>
2020-06-13 11:07:42 +0200

    Código para mantener la sesión de los usuarios que inician sesión

--- Jimena/src/app/iniciar-sesion/iniciar-sesion.component.ts
+++ Jimena/src/app/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -7,14 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniciarSesionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    // Se comprueba si el navegador es compatible con el login
+    if (typeof(Storage) !== 'undefined') {
+      // Código cuando Storage es compatible
+    } else {
+     // Código cuando Storage NO es compatible
+     alert("El navegador utilizado no es compatible con el inicio de sesión. Por favor, pruebe con uno más moderno");
+    }
   }
   public username: string;
   public password: string;
 
   public onSubmit(){
+    // Guardo los datos de inicio en la sesión
+    sessionStorage.setItem('username', this.username);
+    sessionStorage.setItem('password', this.password);
+
+    this.router.navigate(['/principal']);
 
   }
 
--- Jimena/src/app/services/users.service.ts
+++ Jimena/src/app/services/users.service.ts
@@ -8,17 +8,34 @@ import { Observable} from 'rxjs';
 export class UsersService {
 
   userUrl = 'http://timber.gsic.uva.es/sta/data/user/';  // URL to web api
-  //userUrl = 'http://localhost:8888/sta/data/user/';
 
   constructor(private http: HttpClient) { }
 
     /*********************** CREACION DE USUARIOS ******************************/
-    createUser(datos: string, username: string): Observable<string> {
+    public createUser(datos: string, username: string): Observable<string> {
       // Cabecera necesaria
       let urlComplete = this.userUrl+username;
       console.log("La url creada es: "+urlComplete);
       let headers = new HttpHeaders().set('Content-Type','application/json');
       return this.http.put<string>(urlComplete, datos, {headers: headers});
+    }
+
+    /****** Datos de la sesion *******/
+    public getSessionName(): string{
+      let username = sessionStorage.getItem('username');
+      return username;
+    }
+
+    public getUserAutentication(): string{
+      let username = sessionStorage.getItem('username');
+      let password = sessionStorage.getItem('password');
+      let autentication = username+":"+password;
+      console.log(autentication);
+      return autentication;
+    }
+
+    public clearSession(){
+      sessionStorage.clear();
     }
 
 }
