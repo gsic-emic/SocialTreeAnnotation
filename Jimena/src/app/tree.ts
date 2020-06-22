a056a5618df8085ca205ccf0b5510d5863a30e8a
jimenaandrade <jimena.andrade@hotmail.com>
2020-06-22 17:11:53 +0200

    - Añadido carta con la información de una anotación nueva añadida

--- Jimena/src/app/tree.ts
+++ Jimena/src/app/tree.ts
@@ -1,10 +1,10 @@
 // Interfaz con los parámetros de un árbol
 export interface Tree {
       id?: string; // el id contiene la url completa para hacer la consulta sobre el arbol
-      lat: number;
-      long: number;
+      lat?: number;
+      long?: number;
       species?: string;
-      creator: string;
+      creator?: string;
       date?: string;
   }
 
